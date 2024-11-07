3802f3fcf9c0f4a4eef9d47f44b0cda9df4e4551
Serge Manvelov <kawaiier@tutanota.com>
2024-11-07 18:47:20 +0400

    feat: Add animations and CSS styles for shooting game results

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ export default function App() {
   const [selectedWeapon, setSelectedWeapon] = useState("rifle");
   const [selectedBodyPart, setSelectedBodyPart] = useState("head");
   const [result, setResult] = useState("");
+  const [animationClass, setAnimationClass] = useState("");
+
   const weapons = {
     rifle: { head: 60, body: 80, legs: 35, hands: 30 },
     pistol: { head: 35, body: 60, legs: 25, hands: 20 },
     uzi: { head: 30, body: 70, legs: 20, hands: 15 },
     sniper: { head: 80, body: 95, legs: 55, hands: 50 },
   };
+
   function changeWeapon(event) {
     setSelectedWeapon(event.target.value);
   }
+
   function changeBodyPart(event) {
     setSelectedBodyPart(event.target.value);
   }
@@ -22,36 +26,40 @@ export default function App() {
     const randomNumber = getRandomNumber();
     if (randomNumber < weapons[selectedWeapon][selectedBodyPart]) {
       setResult("Hit!");
+      setAnimationClass("animate-hit");
     } else if (randomNumber > weapons[selectedWeapon][selectedBodyPart]) {
       setResult("Miss!");
+      setAnimationClass("animate-miss");
     } else {
       setResult("BAN");
+      setAnimationClass("animate-ban");
     }
   }
+
   function getRandomNumber() {
     return Math.floor(Math.random() * 100 + 1);
   }
+
   return (
     <main className="bg-image-container">
       <div className="container mx-auto">
-        <h1 className="text-center text-green-200 text-4xl  pt-4">
+        <h1 className="text-center text-green-200 text-4xl pt-4">
           Shooting Game
         </h1>
         <div className="flex justify-center gap-4 md:gap-8 mx-4 my-8">
           <div className="flex flex-col">
             <label
-              htmlFor="weaponSpdelect"
-              className="text-green-200 text-base md:text-2xl "
+              htmlFor="weaponSelect"
+              className="text-green-200 text-base md:text-2xl"
             >
               Select a weapon
             </label>
-            {/* TODO: chage color objects */}
             <select
               value={selectedWeapon}
               onChange={changeWeapon}
               name="weapon"
               id="weaponSelect"
-              className=" mb-3"
+              className="mb-3"
             >
               <option value="rifle">Rifle</option>
               <option value="pistol">Pistol</option>
@@ -63,17 +71,16 @@ export default function App() {
           <div className="flex flex-col">
             <label
               htmlFor="bodyPartSelect"
-              className="text-green-200 text-base md:text-2xl "
+              className="text-green-200 text-base md:text-2xl"
             >
               Select a body part
             </label>
-            {/* TODO: chage color objects */}
             <select
               value={selectedBodyPart}
               onChange={changeBodyPart}
               name="bodyPart"
               id="bodyPartSelect"
-              className="  mb-3"
+              className="mb-3"
             >
               <option value="head">Head</option>
               <option value="body">Body</option>
@@ -90,7 +97,11 @@ export default function App() {
             Shoot
           </button>
         </div>
-        <p className="text-slate-100 text-center mt-5 text-xl">{result}</p>
+        <p
+          className={`text-slate-100 text-center mt-5 text-xl ${animationClass}`}
+        >
+          {result}
+        </p>
       </div>
     </main>
   );
